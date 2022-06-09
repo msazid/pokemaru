c7a1e58ae5462a28ff88f6b6f0b14da978997bf6
msazid <mdsaziduli14@gmail.com>
2022-06-09 20:03:35 +0600

    some small changes

--- src/Pages/Pokemon.js
+++ src/Pages/Pokemon.js
@@ -3,7 +3,8 @@ const Pokemon = () => {
     
     return (
         <div>
-           
+           <h1>Pokemaru</h1>
+           <h5>A simple react application.Here you can find your favorite <strong>POKEMON</strong> by select the name.This application build with react.js , tailwindCSS, react-router, Axios</h5>
         </div>
     );
 };
